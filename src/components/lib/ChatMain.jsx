b7e74ecb05afe60d90406f2042410999cc4ddd33
Linkontoask <18875142896@163.com>
2018-11-24 01:08:06 +0800

    发送功能(づ╥﹏╥)づ

--- src/components/lib/ChatMain.jsx
+++ src/components/lib/ChatMain.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import '../style/ChatMain.sass';
 import { callApi } from '../http/fetch'
-import {FixedPage} from "../actions/NavTag";
+// import {FixedPage} from "../actions/NavTag";
 import { LineCose } from "./base/LineCose";
 import EntryCommunication from './EntryCommunication'
 
@@ -33,25 +33,51 @@ class ChatMain extends Component{
                         )
                     })}
                 </div>
-                <EntryCommunication v-model={this.getInfo.bind(this)} />
+                <EntryCommunication keySend={this.getInfo.bind(this)} ref="message" />
             </div>
         )
     }
     componentDidMount() {
         let id = decodeURIComponent(escape(window.atob(this.props.match.params.id))).replace(/\+link$/g, '');
-        const promise = this.jsonLoad(id);
-        console.log(promise)
+        this.jsonLoad(id);
     }
     async jsonLoad (id) {
-        let { json } = await callApi(`http://172.16.1.69:7300/mock/5bf60e808639da0a80f7938b/chatListAll?id=${id}`);
-        this.setState({
-            id: json.data.id,
-            list: json.data.list
-        });
-        this.props.dispatch(FixedPage(json.data.name));
-        console.log(json);
+        // let { json } = await callApi(`http://172.16.1.69:7300/mock/5bf60e808639da0a80f7938b/chatListAll?id=${id}`);
+        // this.setState({
+        //     id: json.data.id,
+        //     list: json.data.list
+        // });
+        // this.props.dispatch(FixedPage(json.data.name));
+        // console.log(json);
     }
     getInfo() {
+        console.log(this.refs.message);
+        this.refs.message.input.value = '';
+        this.sendMessage(this.refs.message.state.value);
+        // this.refs.message.emitEmpty();
+        setTimeout(()=>{
+            this.refs.message.emitEmpty(); // 延迟 清空回车 \n
+        }, 0)
+    }
+    async sendMessage (message) {
+        // let { json } = await callApi(`localhost:8000/message?m=${message}`);
+        /**
+         * 模拟
+         */
+        let json = {ok: 200};
+        if (json.ok === 200) {
+            let list = this.state.list.concat();
+            console.log(list);
+            list.push({
+                target: 0,
+                type: 'normal',
+                content: message
+            });
+            this.setState({
+                ...this.state,
+                list
+            })
+        }
 
     }
 }
--- src/components/lib/EntryCommunication.jsx
+++ src/components/lib/EntryCommunication.jsx
@@ -1,20 +1,46 @@
 import React , { Component } from 'react';
+import PropsTypes from 'prop-types'
 import '../style/EntryCommunication.sass';
 
-import { Icon, Input } from "antd";
+import { Icon, Input  } from "antd";
 const IconFont = Icon.createFromIconfontCN({
     scriptUrl: '//at.alicdn.com/t/font_836575_gxdpqhmux69.js',
 });
-
+const { TextArea } = Input;
 
 export default class EntryCommunication extends Component {
+    constructor() {
+        super();
+        this.state = {
+            value: ''
+        }
+    }
     render() {
+        const { value } = this.state;
         return (
             <div className="EntryCommunication">
                 <IconFont type="icon-keyboard" />
-                <Input placeholder="default size" />
+                <TextArea
+                    autosize={{
+                        maxRows: 1
+                    }}
+                    placeholder="default size"
+                    onPressEnter={this.props.keySend}
+                    onChange={this.onChangeValue.bind(this)}
+                    ref={node => this.input = node}
+                    value={value} />
                 <IconFont type="icon-jiahao-fill" />
             </div>
         )
     }
+    onChangeValue (e) {
+        this.setState({ value: e.target.value });
+    }
+    emitEmpty() {
+        this.setState({ value: '' });
+    }
 }
+
+EntryCommunication.propsTypes = {
+    keySend: PropsTypes.func.isRequired
+};
